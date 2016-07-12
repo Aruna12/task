d7da1ae2348fa135a20ea8c3db7bb9d6b5c6b1ad
Aruna12 <aruaruna1297@gmail.com>
2016-07-12 23:12:19 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,7 @@
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='home.html'>Home</a></li>" +
+"<li><a href='ctf.html'>CTF</a></li>" +
+"<li><a href='guindytimes.html'>GUINDY TIMES</a></li>" +
+"<li><a href='capitalize.html'>CAPITALIZE</a></li>"+
+"</ul>";
